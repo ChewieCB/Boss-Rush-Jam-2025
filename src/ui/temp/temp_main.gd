2a70910833edf2852eb730a79a833d77bbaaedc6
Jack McCaffrey <jackmccaffrey96@gmail.com>
2025-01-04 01:29:18 +0000

    Added quick scene select to temp scene

--- src/ui/temp/temp_main.gd
+++ src/ui/temp/temp_main.gd
@@ -1,8 +1,12 @@
 extends Control
 
-@onready var version_label: RichTextLabel = $PanelContainer/MarginContainer/VBoxContainer/VBoxContainer/BuildNumber
+@onready var version_label: RichTextLabel = $Panel/MarginContainer/VBoxContainer/VBoxContainer/BuildNumber
 
 
 func _ready() -> void:
 	var project_version = ProjectSettings.get_setting("application/config/version")
 	version_label.text = "[center]%s[/center]" % [project_version]
+
+
+func _on_button_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/player/test_scenes/Test_PlayerMovement.tscn")
