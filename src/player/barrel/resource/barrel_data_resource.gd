e24fe5c6f48a01e5add3974485f1543e86e5abe0
NKLam <khoalamvn@gmail.com>
2025-01-14 15:10:36 +1100

    Add a new barrel archetype shotgun spec

--- src/player/barrel/resource/barrel_data_resource.gd
+++ src/player/barrel/resource/barrel_data_resource.gd
@@ -1,6 +1,7 @@
 extends Resource
 class_name BarrelDataResource
 
+## Keep adding at the bottmom, dont add at the middle of list
 enum BarrelIdEnum {
     NONE,
     TEST,
@@ -9,6 +10,7 @@ enum BarrelIdEnum {
     FOOL_ROLLER,
     PROJECTILE_COUNT,
     TRICKSHOT,
+    ARCHETYPE_SHOTGUN_SPEC
 }
 
 @export var barrel_id: BarrelIdEnum
