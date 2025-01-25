a837e02a854e4cabc3e11eaa960a3c8a8b9936f0
Jack McCaffrey <jackmccaffrey96@gmail.com>
2025-01-25 01:27:19 +0000

    Added melee swipe attack

--- src/bosses/slots/boss_slots.gd
+++ src/bosses/slots/boss_slots.gd
@@ -5,7 +5,14 @@ class_name BossSlots
 @onready var projectile_spawn_marker: Marker3D = $MarkerPivot/Marker3D
 @onready var slot_icons_parent: Node3D = $SlotIcons
 
-@export_category("Spin Slots")
+var prev_phase
+
+@export_group("Movement")
+@export var desired_distance: float = 10.0
+@export_subgroup("Orbiting")
+@export var angle_speed = 1.0  # radians/second
+@export var orbit_angle = 0.0  # track this over time
+@export_group("Spin Slots")
 var next_attack: String
 var next_attack_idx: int
 var next_attack_texture: Texture
@@ -22,6 +29,13 @@ var next_attack_texture: Texture
 @export var coin_projectile: PackedScene
 @export var num_bursts: int = 1
 @export var delay_between_burst: float = 0.6
+@export_group("Bell Drop")
+@export var bell_scene: PackedScene
+@export var drop_shadow_material: Material
+@export_group("Lever Swipe")
+@export var swipe_damage: float = 5.0
+@export var swipe_knockback: float = 50.0
+@export var swipe_dodge_speed: float = 10.0
 
 
 func activate() -> void:
@@ -33,20 +47,32 @@ func _physics_process(delta: float) -> void:
 	super(delta)
 	projectile_marker_pivot.look_at(target.global_position)
 	slot_icons_parent.look_at(target.global_position)
+	
+	if hurtbox.monitoring:
+		if target in hurtbox.get_overlapping_bodies():
+			# TODO - add cooldown
+			state_chart.send_event("start_lever_attack")
 
 
 func select_attack_phase_1() -> void:
 	var possible_phases = [
 		# Tuples of event string and icon index 
 		["start_coin_attack", 0],
-		#["start_bell_attack", 1],
+		["start_bell_attack", 1],
 		#"start_lever_attack", # TODO - only triggers in close range as a reaction
 	]
+	if prev_phase:
+		possible_phases.erase(prev_phase)
 	# TODO - add random weighting
 	var new_phase = possible_phases.pick_random()
+	prev_phase = new_phase
 	next_attack = new_phase[0]
 	next_attack_idx = new_phase[1]
 	state_chart.send_event("start_spin_slots")
+
+
+func _on_hurtbox_body_entered(body: Node3D) -> void:
+	pass
 
 
 ### ATTACK PHASES --------------------------------
@@ -65,11 +91,6 @@ func _on_phase_1_state_entered() -> void:
 	current_phase = 1
 	select_attack()
 
-
-# Orbiting movement
-var angle_speed = 1.0  # radians/second
-var orbit_angle = 0.0  # track this over time
-var desired_distance: float = 10.0
 
 func orbit_player(delta) -> void:
 	orbit_angle += angle_speed * delta
@@ -92,7 +113,7 @@ func _on_spin_slots_targeting_state_entered() -> void:
 	
 	state_chart.send_event("start_targeting")
 	state_chart.send_event("attack_buildup")
-	await get_tree().create_timer(2.0).timeout
+	#await get_tree().create_timer(2.0).timeout
 	state_chart.send_event("start_slots")
 
 func _on_spin_slots_spinning_state_entered() -> void:
@@ -112,7 +133,8 @@ func _on_spin_slots_spinning_state_entered() -> void:
 	# Settle each roller one by one
 	for i in range(slot_icons_parent.get_child_count()):
 		while not slot_icons_parent.get_child(i).texture == next_attack_texture:
-			for slot_sprite in slot_icons_parent.get_children():
+			for j in range(i, slot_icons_parent.get_child_count()):
+				var slot_sprite = slot_icons_parent.get_child(j)
 				var sprite_idx: int = slot_icons.find(slot_sprite.texture) + 1
 				var new_idx: int = wrapi(sprite_idx, 0, slot_icons.size() - 1) 
 				slot_sprite.texture = slot_icons[new_idx]
@@ -127,10 +149,7 @@ func _on_spin_slots_recover_state_entered() -> void:
 	debug_state_label.text = "Spin Slots | Recovering"
 	state_chart.send_event("attack_end")
 	
-	for slot_sprite in slot_icons_parent.get_children():
-		slot_sprite.texture = icon_coin
-	
-	await get_tree().create_timer(attack_recovery_time).timeout
+	#await get_tree().create_timer(attack_recovery_time).timeout
 	
 	state_chart.send_event("cooldown_end")
 	state_chart.send_event(next_attack)
@@ -183,17 +202,8 @@ func _on_coin_projectiles_recover_state_entered() -> void:
 	debug_state_label.text = "Coin Burst | Recovering"
 	
 	state_chart.send_event("attack_end")
-	projectile_round_count += 1
-	
 	await get_tree().create_timer(attack_recovery_time).timeout
-	
 	state_chart.send_event("cooldown_end")
-	
-	#if projectile_round_count < max_projectile_rounds:
-		#var fire_again_chance: float = randf()
-		#if fire_again_chance < 0.55:
-			#state_chart.send_event("fire_again")
-			#return
 	
 	select_attack()
 	state_chart.send_event("end_recovery")
@@ -203,6 +213,172 @@ func _on_coin_projectiles_recover_state_entered() -> void:
 # 3 Bells on rollers
 # TODO - Single large AoE bell drops from ceiling
 
+func drop_shadow(
+	target_pos: Vector3,
+	max_radius: float, 
+	drop_time: float = 1.0, 
+	callback: Callable = func(): pass
+) -> void:
+	var debug_mesh_instance = MeshInstance3D.new()
+	var mesh = CylinderMesh.new()
+	
+	# Generate a visual
+	get_tree().get_root().add_child(debug_mesh_instance)
+	
+	debug_mesh_instance.mesh = mesh
+	debug_mesh_instance.cast_shadow = false
+	
+	# Snap to floor
+	var space_state = get_world_3d().direct_space_state
+	var query = PhysicsRayQueryParameters3D.create(
+		target_pos, 
+		target_pos - Vector3(0, 100, 0),
+		pow(2, 1-1) + pow(2, 7-1)
+	)
+	var result = space_state.intersect_ray(query)
+	if result:
+		target_pos.y = result.position.y
+	
+	debug_mesh_instance.global_position = target_pos
+	
+	mesh.bottom_radius = 0.0
+	mesh.top_radius = 0.0
+	mesh.height = 0.5
+	mesh.material = drop_shadow_material
+	
+	var shadow_tween = get_tree().create_tween()
+	
+	shadow_tween.tween_property(debug_mesh_instance, "mesh:bottom_radius", max_radius, drop_time)#.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CIRC)
+	shadow_tween.parallel().tween_property(debug_mesh_instance, "mesh:top_radius", max_radius, drop_time)#.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CIRC)
+	shadow_tween.tween_callback(debug_mesh_instance.queue_free)
+
+
+func _on_bell_drop_state_entered() -> void:
+	desired_distance = 35.0
+
+func _on_bell_drop_state_physics_processing(delta: float) -> void:
+	orbit_player(delta)
+
+func _on_bell_drop_state_exited() -> void:
+	desired_distance = 10.0
+
+
+func _on_bell_drop_targeting_state_entered() -> void:
+	debug_state_label.text = "Bell Drop | Targeting"
+	
+	state_chart.send_event("start_moving")
+	state_chart.send_event("attack_buildup")
+	await get_tree().create_timer(1.2).timeout
+	state_chart.send_event("start_drop")
+
+
+func _on_bell_drop_dropping_state_entered() -> void:
+	debug_state_label.text = "Bell Drop | Dropping"
+	
+	state_chart.send_event("attack_telegraph")
+	await get_tree().create_timer(telegraph_time).timeout
+	state_chart.send_event("attack_start")
+	
+	# TODO - spawn shadow/mesh to show AoE, grow in size as it drops
+	drop_shadow(target.global_position, 8.0, 4.0)
+	# TODO - spawn mesh that drops down and crashes, dealing damage
+	#
+	state_chart.send_event("finish_drop")
+
+
+func _on_bell_drop_recover_state_entered() -> void:
+	debug_state_label.text = "Bell Drop | Recovering"
+	
+	state_chart.send_event("attack_end")
+	await get_tree().create_timer(attack_recovery_time).timeout
+	state_chart.send_event("cooldown_end")
+	
+	select_attack()
+	state_chart.send_event("end_recovery")
+
+
+#### LEVER SWIPE
+# TODO - Whacks player with slot level arm if they get too close
+func _on_lever_swipe_targeting_state_entered() -> void:
+	debug_state_label.text = "Lever Swipe | Targeting"
+	
+	state_chart.send_event("start_targeting")
+	state_chart.send_event("attack_telegraph")
+	await get_tree().create_timer(telegraph_time).timeout
+	hurtbox.set_deferred("monitoring", true)
+	state_chart.send_event("start_swipe")
+
+
+func _on_lever_swipe_swipe_state_entered() -> void:
+	debug_state_label.text = "Lever Swipe | Swipe"
+	
+	state_chart.send_event("attack_start")
+	sprite.modulate = Color.ORANGE
+	
+	# Hit player in melee range and flee away
+	if target in hurtbox.get_overlapping_bodies():
+		target.health_component.damage(swipe_damage)
+		hurtbox.set_deferred("monitoring", false)
+		# TODO - fix this knockback
+		var knockback_vector = -self.global_basis.z * swipe_knockback
+		target.velocity.x = 0
+		target.velocity.z = 0
+		target.velocity += knockback_vector
+		
+		var dodge_vector = self.global_basis.z * swipe_dodge_speed
+		
+		# Make sure we don't dodge into a wall
+		var space_state = get_world_3d().direct_space_state
+		var dodge_collisions = []
+		for angle in [0, PI/2, PI, 3 * PI/2]:
+			dodge_vector = dodge_vector.rotated(Vector3.UP, angle)
+			var query = PhysicsRayQueryParameters3D.create(
+				self.global_position, 
+				self.global_position + dodge_vector,
+				pow(2, 1-1) + pow(2, 7-1)
+			)
+			var result = space_state.intersect_ray(query)
+			if result:
+				dodge_collisions.append(dodge_vector)
+				continue
+			else:
+				break
+		
+		# If all options collide, pick the furthest direction to dodge in
+		if dodge_collisions.size() == 4:
+			dodge_collisions.sort_custom(
+				func(a, b):
+					var a_dist: float = self.global_position.distance_to(a)
+					var b_dist: float = self.global_position.distance_to(b)
+					if a_dist > b_dist:
+						return true
+					return false
+			)
+			dodge_vector = dodge_collisions.front()
+		
+		self.velocity.x = dodge_vector.x
+		self.velocity.z = dodge_vector.z
+	
+	await get_tree().create_timer(0.2).timeout
+	sprite.modulate = Color.WHITE
+	
+	state_chart.send_event("end_swipe")
+
+
+func _on_lever_swipe_recover_state_entered() -> void:
+	debug_state_label.text = "Lever Swipe | Recovery"
+	hurtbox.set_deferred("monitoring", true)
+	
+	state_chart.send_event("attack_end")
+	await get_tree().create_timer(attack_recovery_time).timeout
+	state_chart.send_event("cooldown_end")
+	
+	select_attack()
+	state_chart.send_event("end_recovery")
+
+
+
+
 #### DIAMOND SCATTERSHOT
 # 3 Diamonds on rollers
 # TODO - Homing diamond projectile, fired in a spiral/radial pattern from boss
@@ -211,8 +387,6 @@ func _on_coin_projectiles_recover_state_entered() -> void:
 # 3 BARs on rollers
 # TODO - Existing charge behaviour
 
-#### LEVER SWIPE
-# TODO - Whacks player with slot level arm if they get too close
 
 #### CHERRY BOMB
 # 3 Cherries on rollers
@@ -253,74 +427,6 @@ func _on_phase_chase_player_recover_state_entered() -> void:
 	select_attack()
 	state_chart.send_event("end_recovery")
 
-
-#### RANGED PROJECTILES
-func _on_phase_ranged_projectiles_move_to_center_state_entered() -> void:
-	sprite.modulate = Color.YELLOW
-	debug_state_label.text = "Projectiles | Moving"
-	
-	MAX_SPEED *= 2
-	
-	var valid_move_points = ranged_move_points.duplicate()
-	valid_move_points.sort_custom(
-		func(a, b):
-			var a_dist: float = self.global_position.distance_to(a.global_position)
-			var b_dist: float = self.global_position.distance_to(b.global_position)
-			if a_dist < b_dist:
-				return true
-			return false
-	)
-	valid_move_points.pop_front()
-	
-	cached_target = target
-	if valid_move_points:
-		target = valid_move_points[0]
-		state_chart.send_event("start_moving")
-		
-		await navigation_component.nav_agent.navigation_finished
-		state_chart.send_event("start_projectiles")
-	else:
-		select_attack()
-
-func _on_phase_ranged_projectiles_move_to_center_state_exited() -> void:
-	target = cached_target
-	MAX_SPEED /= 2
-
-func _on_phase_ranged_projectiles_fire_projectiles_state_entered() -> void:
-	sprite.modulate = Color.ORANGE
-	debug_state_label.text = "Projectiles | Firing"
-	
-	state_chart.send_event("start_targeting")
-	
-	for i in projectiles_per_phase:
-		await get_tree().create_timer(delay_per_projectile).timeout
-		var projectile: TestProjectile = projectile_scene.instantiate()
-		get_parent().get_parent().add_child(projectile)
-		projectile.global_position = self.global_position + Vector3(0, 3, 0)
-		projectile.look_at(target.global_position, Vector3.UP)
-		SoundManager.play_sound(TEMP_sfx_projectile)
-	
-	await get_tree().create_timer(delay_per_projectile).timeout
-	state_chart.send_event("end_projectiles")
-
-func _on_phase_ranged_projectiles_recover_state_entered() -> void:
-	sprite.modulate = Color.YELLOW
-	debug_state_label.text = "Projectiles | Recovering"
-	
-	state_chart.send_event("attack_end")
-	projectile_round_count += 1
-	await get_tree().create_timer(attack_recovery_time).timeout
-	state_chart.send_event("cooldown_end")
-	
-	if projectile_round_count < max_projectile_rounds:
-		var fire_again_chance: float = randf()
-		if fire_again_chance < 0.55:
-			state_chart.send_event("fire_again")
-			return
-	
-	ranged_phase_count += 1
-	select_attack()
-	state_chart.send_event("change_position")
 
 #### AREA DENIAL
 func _on_phase_area_denial_move_to_center_state_entered() -> void:
