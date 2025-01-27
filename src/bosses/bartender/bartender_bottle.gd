d3a1c845a9d9c580cb67b9c54332c26faad282ab
NKLam <khoalamvn@gmail.com>
2025-01-27 17:27:09 +0700

    Add throw bottle attack

---
+++ src/bosses/bartender/bartender_bottle.gd
@@ -0,0 +1,42 @@
+extends RigidBody3D
+
+@onready var life_timer: Timer = $LifeTimer
+@export var break_effect_prefab: PackedScene
+
+var damage
+var projectile_speed = 100
+var current_dir
+
+func init(start_pos: Vector3, dir: Vector3, _damage: int, _speed: float):
+	life_timer.start()
+	projectile_speed = _speed
+	damage = _damage
+	current_dir = dir.normalized()
+	look_at_from_position(start_pos, start_pos + dir)
+
+	await get_tree().physics_frame
+	await get_tree().physics_frame
+
+	apply_impulse(current_dir * projectile_speed, Vector3.ZERO)
+
+
+func _on_life_timer_timeout() -> void:
+	call_deferred("queue_free")
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body is CharacterBody3D:
+		body.health_component.damage(damage)
+		spawn_break_effect()
+	else:
+		if body is Shield:
+			body.impact(self.global_position)
+			body.health_component.damage(damage)
+		elif body is RouletteBall:
+			body.health_component.damage(damage)
+		spawn_break_effect()
+	call_deferred("queue_free")
+
+func spawn_break_effect():
+	if break_effect_prefab == null:
+		return
+	return
--- src/bosses/bartender/boss_bartender.gd
+++ src/bosses/bartender/boss_bartender.gd
@@ -6,14 +6,15 @@ extends BossCore
 
 @export_category("Attacks")
 @export var shotgun_proj_prefab: PackedScene
+@export var empty_bottle_prefab: PackedScene
 @export var molotov_prefab: PackedScene
-@export var poison_smoke_prefab: PackedScene
-@export var slow_smoke_prefab: PackedScene
+@export var poison_bottle_prefab: PackedScene
+@export var slow_bottle_prefab: PackedScene
 
 @onready var proj_spawn_marker = $ProjectileSpawnPos
 
 var previous_attack: String
-var force_shotgun_knockback = false
+var player_is_near = false
 var proj_spawn_pos
 
 const DIFFICULTY_LV = 1
@@ -62,11 +63,13 @@ func select_attack() -> void:
 			push_error("Invalid phase %s" % current_phase)
 
 func select_attack_phase_1() -> void:
-	if force_shotgun_knockback:
-		state_chart.send_event("start_shotgun_blast")
-		force_shotgun_knockback = false
-		previous_attack = "start_shotgun_blast"
-		return
+	# If player is near, more likely to use shotgun blast
+	if player_is_near:
+		var roll = randi_range(0, 100)
+		if roll <= 75:
+			state_chart.send_event("start_shotgun_blast")
+			previous_attack = "start_shotgun_blast"
+			return
 
 	var possible_attacks = [
 		"start_throw_broken_bottle",
@@ -93,17 +96,22 @@ func _on_died() -> void:
 #### Any Phase
 
 func _on_idle_state_entered() -> void:
+	debug_state_label.text = "Idle"
+	await get_tree().create_timer(1).timeout
 	select_attack()
 
 # Shotgun blast
 
 func shotgun_blast():
 	debug_state_label.text = "Shotgun blast"
-	var proj_amount = 6
-	var proj_damage = 2
+	var proj_amount = 8
+	var proj_damage = 3
 	var proj_speed = 40
-	var n_shot_repeat = 1
-	var spread_angle = 4
+	var n_shot_repeat = current_phase
+	if player_is_near:
+		n_shot_repeat += 1
+	var spread_angle = 6
+	var delay_between_burst = 0.5
 	state_chart.send_event("attack_telegraph")
 	await get_tree().create_timer(telegraph_time).timeout
 	state_chart.send_event("attack_start")
@@ -114,7 +122,9 @@ func shotgun_blast():
 			var bullet_inst = shotgun_proj_prefab.instantiate()
 			get_parent().add_child(bullet_inst)
 			bullet_inst.init(proj_spawn_pos, spreaded_direction, proj_damage, proj_speed)
-	state_chart.send_event("attack_end")
+		if n_shot_repeat > 1 and i < n_shot_repeat - 1:
+			await get_tree().create_timer(delay_between_burst).timeout
+	state_chart.send_event("attack_end_now")
 	state_chart.send_event("return_idle")
 
 func _on_shotgun_blast_state_entered() -> void:
@@ -122,46 +132,52 @@ func _on_shotgun_blast_state_entered() -> void:
 
 #### Phase 1
 
+func throw_bottle(prefab: PackedScene, n_bottle_repeat = 1, spread_angle = 0):
+	var proj_damage = 10
+	var throw_force = proj_spawn_pos.distance_to(target.global_position) * 1.2
+	var aim_direction = proj_spawn_pos.direction_to(target.global_position)
+	aim_direction += Vector3(0, 0.2, 0) # Make it upward a bit
+	var modified_spawn_pos = proj_spawn_pos + aim_direction # Avoid stuck inside boss body
+	for i in range(n_bottle_repeat):
+		var bottle_inst = prefab.instantiate()
+		get_parent().add_child(bottle_inst)
+		var spreaded_direction = GunUtils.get_spread_direction(aim_direction, spread_angle)
+		bottle_inst.init(modified_spawn_pos, spreaded_direction, proj_damage, throw_force)
+
 func _on_throw_broken_bottle_state_entered() -> void:
-	debug_state_label.text = "Throw borken bottle"
-	state_chart.send_event("attack_telegraph")
-	await get_tree().create_timer(telegraph_time).timeout
-	state_chart.send_event("attack_start")
-	
-	# Do sth here
-	await get_tree().create_timer(0.5).timeout
-	state_chart.send_event("attack_end")
+	debug_state_label.text = "Throw broken bottle"
+	await get_tree().create_timer(1).timeout
+	throw_bottle(empty_bottle_prefab, 3, 8)
+	await get_tree().create_timer(1).timeout
 	state_chart.send_event("return_idle")
 
 
 func _on_throw_concoction_state_entered() -> void:
 	debug_state_label.text = "Throw concoction"
-	state_chart.send_event("attack_telegraph")
-	await get_tree().create_timer(telegraph_time).timeout
-	state_chart.send_event("attack_start")
+	await get_tree().create_timer(1).timeout
+	# Do sth
+	throw_bottle(empty_bottle_prefab)
 
-	# Do sth here
-	await get_tree().create_timer(0.5).timeout
-	state_chart.send_event("attack_end")
+	await get_tree().create_timer(1).timeout
 	state_chart.send_event("return_idle")
 
 
 func _on_brew_drink_state_entered() -> void:
 	debug_state_label.text = "Brew drink"
-	state_chart.send_event("attack_telegraph")
-	await get_tree().create_timer(telegraph_time).timeout
-	state_chart.send_event("attack_start")
+	await get_tree().create_timer(1).timeout
+	# Do sth
+	throw_bottle(empty_bottle_prefab)
 
-	# Do sth here
-	await get_tree().create_timer(0.5).timeout
-	state_chart.send_event("attack_end")
+	await get_tree().create_timer(1).timeout
 	state_chart.send_event("return_idle")
 
 
 ### Others
 
-func _on_shotgun_trigger_area_body_entered(body:Node3D) -> void:
+func _on_shotgun_trigger_area_body_entered(body: Node3D) -> void:
 	if body is Player:
-		force_shotgun_knockback = true
+		player_is_near = true
 
-
+func _on_shotgun_trigger_area_body_exited(body: Node3D) -> void:
+	if body is Player:
+		player_is_near = false
