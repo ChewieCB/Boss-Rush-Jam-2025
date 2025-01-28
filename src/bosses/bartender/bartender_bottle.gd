76f26378a300f320d949e0e13652ec1520d93f5e
NKLam <khoalamvn@gmail.com>
2025-01-28 20:56:42 +0700

    Add speed and strength drink, add barrel throwing to bartender boss

--- src/bosses/bartender/bartender_bottle.gd
+++ src/bosses/bartender/bartender_bottle.gd
@@ -1,6 +1,7 @@
 extends RigidBody3D
 
 @export var break_effect_prefab: PackedScene
+@export var break_on_contact = true
 
 @onready var life_timer: Timer = $LifeTimer
 
@@ -36,7 +37,8 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 			body.health_component.damage(damage)
 
 	spawn_break_effect()
-	call_deferred("queue_free")
+	if break_on_contact:
+		call_deferred("queue_free")
 
 func spawn_break_effect():
 	if break_effect_prefab == null:
--- src/bosses/bartender/boss_bartender.gd
+++ src/bosses/bartender/boss_bartender.gd
@@ -11,13 +11,21 @@ extends BossCore
 @export var poison_bottle_prefab: PackedScene
 @export var slow_bottle_prefab: PackedScene
 @export var heal_bottle_prefab: PackedScene
+@export var beer_barrel_prefab: PackedScene
+@export var bottle_damage = 10
+@export var barrel_damage = 45
 
 @export_category("Drinks")
 @export var defense_icon: Texture2D
+@export var speed_icon: Texture2D
+@export var strength_icon: Texture2D
+
 ## Received damage will multiply with this value
 @export var defense_buff_resistance = 0.5
+@export var speed_buff_modifier = 0.5
 
 @export_category("Movement")
+@export var base_movespeed = 10
 @export var behind_bar_move_points: Array[Marker3D] = []
 
 @onready var buff_expire_timer: Timer = $BuffExpireTimer
@@ -27,11 +35,15 @@ extends BossCore
 var previous_attack: String
 var player_is_near = false
 var current_buff: String = ""
+var current_speed_modifier = 1
+var current_delay_modifier = 1
+var has_strength_buff = false
 
 const DIFFICULTY_LV = 1
 
 func _ready() -> void:
 	super()
+	navigation_component.current_speed = base_movespeed * current_speed_modifier
 
 
 func _process(delta: float) -> void:
@@ -90,6 +102,12 @@ func select_attack_phase_1() -> void:
 		"start_throw_heal_bottle"
 	]
 
+	# More likely to throw bottle / throw barrel when has str buff
+	var throw_barrel_bonus_freq = 5
+	if has_strength_buff:
+		for i in range(throw_barrel_bonus_freq):
+			possible_attacks.append("start_throw_broken_bottle")
+
 	# Avoid use same attack twice in a row (except concoction)
 	if previous_attack:
 		possible_attacks.erase(previous_attack)
@@ -98,6 +116,7 @@ func select_attack_phase_1() -> void:
 	previous_attack = chosen_attack
 
 	var move_point = get_behind_bar_move_point()
+	navigation_component.current_speed = base_movespeed * current_speed_modifier
 	if move_point:
 		navigation_component.target = move_point
 		state_chart.send_event("start_moving")
@@ -154,14 +173,16 @@ func _on_phase_1_idle_state_entered() -> void:
 	await get_tree().create_timer(1).timeout
 	select_attack()
 
-func throw_bottle(prefab: PackedScene, n_bottle_repeat = 1, spread_angle = 0):
-	var proj_damage = 10
+func throw_bottle(prefab: PackedScene, n_bottle_repeat = 1, spread_angle = 0, proj_damage = 10):
 	var throw_force = proj_spawn_marker.global_position.distance_to(target.global_position)
 	# Magic number that make bartender throw better
 	if throw_force >= 30:
 		throw_force *= 0.8
 	var aim_direction = proj_spawn_marker.global_position.direction_to(target.global_position)
-	aim_direction += Vector3(0, 0.2, 0) # Make it upward a bit
+	if has_strength_buff:
+		throw_force *= 2
+	else:
+		aim_direction += Vector3(0, 0.2, 0) # Make it upward a bit
 	aim_direction = aim_direction.normalized()
 	var modified_spawn_pos = proj_spawn_marker.global_position + aim_direction # Avoid stuck inside boss body
 	for i in range(n_bottle_repeat):
@@ -189,7 +210,11 @@ func throw_concoction_bottle():
 		slow_bottle_prefab,
 	]
 	var chosen_prefab = possible_bottle_prefab.pick_random()
-	var n_bottle = current_phase
+	var n_bottle = 1
+	if current_phase > 1:
+		n_bottle += 1
+	if has_strength_buff:
+		n_bottle += 1
 	var spread_angle = (n_bottle - 1) * 20
 	throw_bottle(chosen_prefab, n_bottle, spread_angle)
 
@@ -197,52 +222,66 @@ func throw_concoction_bottle():
 func brew_drink():
 	var possible_drink = [
 		"defense",
-		# "heal",
-		# "speed",
-		# "strength",
+		"speed",
+		"strength",
 	]
 	var chosen_drink = possible_drink.pick_random()
 	# I want boss to stack buffs, but due to time constraint (and possible bugs)
-	# boss can only just have 1 buff ongoing
+	# boss can only just have 1 buff ongoing for now
 	reset_buff()
+	current_buff = chosen_drink
 	match chosen_drink:
 		"defense":
 			status_icon.texture = defense_icon
 			health_component.modified_resistance = defense_buff_resistance
 			buff_expire_timer.start()
+		"speed":
+			status_icon.texture = speed_icon
+			current_speed_modifier = 1 + speed_buff_modifier
+			current_delay_modifier = 1 - speed_buff_modifier
+			navigation_component.current_speed = base_movespeed * current_speed_modifier
+			buff_expire_timer.start()
+		"strength":
+			has_strength_buff = true
+			status_icon.texture = strength_icon
+			buff_expire_timer.start()
 
+## If has str buff, throw barrel instead
 func _on_throw_broken_bottle_state_entered() -> void:
 	debug_state_label.text = "Throw broken bottle"
-	await get_tree().create_timer(0.25).timeout
-	var n_bottle = randi_range(3, 6)
-	var spread = randf_range(4, 10)
+	await get_tree().create_timer(0.25 * current_delay_modifier).timeout
 	# TODO: Change sprite "Throw"
-	throw_bottle(empty_bottle_prefab, n_bottle, spread)
-	await get_tree().create_timer(0.25).timeout
+	if has_strength_buff:
+		throw_bottle(beer_barrel_prefab, 1, 1, barrel_damage)
+	else:
+		var n_bottle = randi_range(2, 4)
+		var spread = randf_range(5, 10)
+		throw_bottle(empty_bottle_prefab, n_bottle, spread, bottle_damage)
+	await get_tree().create_timer(0.25 * current_delay_modifier).timeout
 	state_chart.send_event("return_idle")
 
 
 func _on_throw_concoction_state_entered() -> void:
 	debug_state_label.text = "Throw concoction"
-	await get_tree().create_timer(0.25).timeout
+	await get_tree().create_timer(0.25 * current_delay_modifier).timeout
 	# TODO: Change sprite "Throw"
 	throw_concoction_bottle()
-	await get_tree().create_timer(0.25).timeout
+	await get_tree().create_timer(0.25 * current_delay_modifier).timeout
 	state_chart.send_event("return_idle")
 
 
 func _on_brew_drink_state_entered() -> void:
 	debug_state_label.text = "Brew drink"
-	await get_tree().create_timer(1).timeout
+	await get_tree().create_timer(1 * current_delay_modifier).timeout
 	# TODO: Change sprite "Brew"
 	brew_drink()
-	await get_tree().create_timer(0.25).timeout
+	await get_tree().create_timer(0.25 * current_delay_modifier).timeout
 	state_chart.send_event("return_idle")
 
 
 func _on_throw_heal_bottle_state_entered() -> void:
 	debug_state_label.text = "Throw heal bottle"
-	await get_tree().create_timer(0.25).timeout
+	await get_tree().create_timer(0.25 * current_delay_modifier).timeout
 	throw_heal_bottle()
 	await get_tree().create_timer(1.5).timeout
 	state_chart.send_event("return_idle")
@@ -260,8 +299,12 @@ func _on_shotgun_trigger_area_body_exited(body: Node3D) -> void:
 		player_is_near = false
 
 func reset_buff():
+	current_buff = ""
 	status_icon.texture = null
 	health_component.modified_resistance = 1
+	current_speed_modifier = 1
+	current_delay_modifier = 1
+	has_strength_buff = false
 
 
 func _on_buff_expire_timer_timeout() -> void:
