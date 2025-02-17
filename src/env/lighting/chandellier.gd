33d0cf2b662a87b276f281dbb16022b89b691643
NKLam <khoalamvn@gmail.com>
2025-02-17 22:27:39 +1100

    Make chandelier breakable and tweak bartender map baked light

--- src/env/lighting/chandellier.gd
+++ src/env/lighting/chandellier.gd
@@ -1,11 +1,7 @@
 extends StaticBody3D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var health_component = $HealthComponent
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_health_component_died() -> void:
+	call_deferred("queue_free")
