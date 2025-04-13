01d63a5b7c1568d4b10fbd4cfc80338466eabb68
Jack McCaffrey <jackmccaffrey96@gmail.com>
2025-04-13 17:21:23 +0100

    Added animation and damage zone to rising water/platforms

--- src/maps/chip_boss/boss_chip.gd
+++ src/maps/chip_boss/boss_chip.gd
@@ -5,6 +5,15 @@ extends Node3D
 
 @onready var func_godot_parent: FuncGodotMap = $FuncGodotMap
 @onready var worldspawn_mesh: StaticBody3D = func_godot_parent.find_child("entity_0_worldspawn")
+
+@export var lower_water_level: float = -4.6
+@export var upper_water_level: float = -2.6
+@export var lower_platform_level: float = -6.0
+@export var upper_platform_level: float = -4.0
+@onready var waterfalls: Node3D = $Waterfalls
+@onready var water_surface: MeshInstance3D = $WaterSurfaceMesh
+@export var rising_platforms: Array[Node3D]
+
 @onready var win_ui: Control = $UI/BossDefeatedUI
 @export var win_subtext: Array[String]
 @export var lose_tips: Array[String]
@@ -21,6 +30,10 @@ extends Node3D
 
 var nav_region: NavigationRegion3D
 
+@onready var water_damage_timer: Timer = $WaterDamageTimer
+@export var water_damage_tick: float = 0.6
+@export var water_damage_amount: float = 5.0
+
 
 func _ready() -> void:
 	if GameManager.cached_player_pos_relative_to_elevator_doors:
@@ -28,6 +41,11 @@ func _ready() -> void:
 		player.global_position = player_start_pos
 		player.rotation = GameManager.cached_player_rotation
 		player.player_camera.rotation = GameManager.cached_camera_rotation
+	
+	waterfalls.visible = false
+	water_surface.global_position.y = lower_water_level
+	for platform in rising_platforms:
+		platform.global_position.y = lower_platform_level
 	
 	elevator_doors.open()
 	
@@ -37,8 +55,11 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("input_1"):
-		if breakable_floor:
-			breakable_floor.queue_free()
+		#if breakable_floor:
+			#breakable_floor.queue_free()
+		raise_water()
+	elif event.is_action_pressed("input_2"):
+		lower_water()
 
 
 func _on_boss_trigger_volume_body_entered(body: Node3D) -> void:
@@ -69,3 +90,38 @@ func _rebake_nav() -> void:
 	if nav_region.is_baking():
 		await nav_region.bake_finished
 	nav_region.bake_navigation_mesh()
+
+
+func raise_water() -> void:
+	waterfalls.visible = true
+	var water_tween: Tween = get_tree().create_tween()
+	water_tween.tween_property(water_surface, "global_position:y", upper_water_level, 1.4)
+	for platform in rising_platforms:
+		water_tween.parallel().tween_property(platform, "global_position:y", upper_platform_level, 1.4)
+	
+	await water_tween.finished
+
+
+func lower_water() -> void:
+	waterfalls.visible = false
+	var water_tween: Tween = get_tree().create_tween()
+	water_tween.tween_property(water_surface, "global_position:y", lower_water_level, 1.4)
+	for platform in rising_platforms:
+		water_tween.parallel().tween_property(platform, "global_position:y", lower_platform_level, 1.4)
+	
+	await water_tween.finished
+
+
+func _on_water_damage_area_body_entered(body: Node3D) -> void:
+	if body is Player:
+		player.health_component.damage(water_damage_amount)
+		water_damage_timer.start(water_damage_tick)
+
+
+func _on_water_damage_area_body_exited(body: Node3D) -> void:
+	if body is Player:
+		water_damage_timer.stop()
+
+
+func _on_water_damage_timer_timeout() -> void:
+	player.health_component.damage(water_damage_amount)
