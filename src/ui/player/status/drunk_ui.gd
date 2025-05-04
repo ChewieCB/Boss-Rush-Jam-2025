7972a426ec1cd1ea03067b4d15ca449980e891bd
Jack McCaffrey <jackmccaffrey96@gmail.com>
2025-05-04 15:21:33 +0100

    Improved radial blur tweening

--- src/ui/player/status/drunk_ui.gd
+++ src/ui/player/status/drunk_ui.gd
@@ -2,24 +2,43 @@ extends Control
 
 
 @onready var rect: ColorRect = $ColorRect
+var drunk_tween: Tween
 @export var lower_blur: float = 0.015
-@export var upper_blur: float = 0.04
+@export var upper_blur: float = 0.03
 
 
 func start_drunk() -> void:
-	rect.visible = true
-	var drunk_tween: Tween = get_tree().create_tween()
+	drunk_tween = create_tween()
+	drunk_tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
+	drunk_tween.tween_method(
+		_set_blur_amount, 0.0, lower_blur, 0.6
+	).set_ease(Tween.EASE_IN)
+	drunk_tween.parallel().tween_property(
+		rect, "color:a", 1.0, 0.6
+	).set_ease(Tween.EASE_IN)
+	await drunk_tween.finished
+	drunk_tween = create_tween()
+	drunk_tween.chain().tween_method(
+		_set_blur_amount, lower_blur, upper_blur, 0.8
+	).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
+	drunk_tween.chain().tween_method(
+		_set_blur_amount, upper_blur, lower_blur, 0.8
+	).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
 	drunk_tween.set_loops()
-	drunk_tween.chain().tween_method(
-		_set_blur_amount, lower_blur, upper_blur, 1.2
-	).set_trans(Tween.TRANS_CIRC)#.set_ease(Tween.EASE_IN)
-	drunk_tween.chain().tween_method(
-		_set_blur_amount, upper_blur, lower_blur, 1.2
-	).set_trans(Tween.TRANS_CIRC)#.set_ease(Tween.EASE_IN)
 
 
 func end_drunk() -> void:
-	rect.visible = false
+	if drunk_tween:
+		drunk_tween.kill()
+	drunk_tween = create_tween()
+	
+	var current_blur = rect.material.get("shader_parameter/blur_power")
+	drunk_tween.tween_method(
+		_set_blur_amount, current_blur, 0.0, 0.3
+	).set_ease(Tween.EASE_OUT)
+	drunk_tween.parallel().tween_property(
+		rect, "color:a", 0.0, 0.6
+	).set_ease(Tween.EASE_OUT)
 
 
 func _set_blur_amount(amount: float) -> void:
