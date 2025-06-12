c605ab7a8574b75b463ec2517bb664754887f604
Jack McCaffrey <jackmccaffrey96@gmail.com>
2025-06-12 19:32:22 +0100

    Tutorial boss room blockout

---
+++ src/maps/tutorial/tutorial_boss_map.gd
@@ -0,0 +1,29 @@
+extends BossMap
+
+@export var boss_doors: ElevatorDoors
+@export var exit_doors: ElevatorDoors
+
+
+func _ready() -> void:
+	super()
+	boss_doors.close()
+
+
+func _on_boss_door_trigger_volume_body_entered(body: Node3D) -> void:
+	if body is Player:
+		boss_doors.open()
+
+
+func _on_boss_door_trigger_volume_body_exited(body: Node3D) -> void:
+	if body is Player:
+		boss_doors.close()
+
+
+func _on_elevator_door_trigger_volume_body_entered(body: Node3D) -> void:
+	if body is Player:
+		exit_doors.open()
+
+
+func _on_elevator_door_trigger_volume_body_exited(body: Node3D) -> void:
+	if body is Player:
+		exit_doors.close()
