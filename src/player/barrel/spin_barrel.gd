6e38fbcf3f00c276ce4cf5b122596663714cb53c
Jack McCaffrey <jackmccaffrey96@gmail.com>
2025-06-15 22:26:04 +0100

    Improved barrel spin randomisation feel

--- src/player/barrel/spin_barrel.gd
+++ src/player/barrel/spin_barrel.gd
@@ -16,7 +16,8 @@ var chosen_id: int:
 		chosen_id = value
 		barrel_effect_changed.emit(self, effect_list[chosen_id])
 var is_equipped = false
-var last_chosen_id = 0
+var last_chosen_queue = []
+
 
 func _ready() -> void:
 	for child in effect_container.get_children():
@@ -26,16 +27,24 @@ func _ready() -> void:
 			child.owner_barrel = self
 			effect_list.append(child)
 	chosen_id = 0
+	effect_list.shuffle()
 	instant_spin()
 
 
 func start_spin():
 	is_spinning = true
 
+
 func stop_spin():
 	is_spinning = false
-	prevent_roll_same_effect()
-	last_chosen_id = chosen_id
+	#prevent_roll_same_effect()
+	chosen_id = get_less_used_effects()
+	# When we pick a new effect, move it to the front of the last used queue 
+	# so we can deprioritise it next spin
+	if chosen_id in last_chosen_queue:
+		last_chosen_queue.pop_at(last_chosen_queue.find(chosen_id))
+	last_chosen_queue.push_front(chosen_id)
+
 
 func _process(delta: float) -> void:
 	if not is_spinning:
@@ -45,6 +54,7 @@ func _process(delta: float) -> void:
 	if spin_interval_timer > SPIN_INTERVAL:
 		spin_interval_timer = 0
 		chosen_id = randi_range(0, len(effect_list) - 1)
+		#chosen_id = get_less_used_effects()
 
 
 func instant_spin():
@@ -54,11 +64,38 @@ func instant_spin():
 func get_active_effect():
 	return effect_list[chosen_id]
 
-func prevent_roll_same_effect():
-	if chosen_id == last_chosen_id:
-		var filtered_id_array = []
-		for i in range(len(effect_list)):
-			if i != chosen_id:
-				filtered_id_array.append(i)
-		if filtered_id_array.size() > 0:
-			chosen_id = filtered_id_array.pick_random()
+
+func get_less_used_effects() -> int:
+	var effect_id: int = -1
+	var chance: float = randf()
+	match last_chosen_queue.size():
+		# If we haven't chosen any effects, pick one at random
+		0:
+			effect_id = randi_range(0, len(effect_list) - 1)
+		# If we have chosen all effects previously, pick the least recent effect
+		3:
+			# 20% chance to get same barrel
+			if chance <= 0.4:
+				effect_id = last_chosen_queue.front()
+			elif chance <= 0.7:
+				effect_id = last_chosen_queue[1]
+			else:
+				effect_id = last_chosen_queue.back()
+		# If we've chosen some, but not all, effects before, pick a random effect we haven't chosen yet
+		_:
+			# 20% chance to get same barrel
+			if chance <= 0.2:
+				effect_id = last_chosen_queue.front()
+			else:
+				var unused_effects = range(0, len(effect_list))
+				unused_effects = unused_effects.filter(
+					func(id):
+						return id not in last_chosen_queue
+				)
+				if chance <= 0.6:
+					effect_id = unused_effects.front()
+				else:
+					effect_id = unused_effects.back()
+				#effect_id = unused_effects.pick_random()
+	
+	return effect_id
