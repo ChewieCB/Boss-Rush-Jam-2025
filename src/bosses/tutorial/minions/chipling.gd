ed5819ddc7533663453573e41838b18b278530bf
Jack McCaffrey <jackmccaffrey96@gmail.com>
2025-06-21 15:30:14 +0100

    Added chipling spawning to first area

--- src/bosses/tutorial/minions/chipling.gd
+++ src/bosses/tutorial/minions/chipling.gd
@@ -3,9 +3,18 @@ class_name Chipling
 
 @export var spark_scene: PackedScene
 
+@export_group("Spawning")
+#@export var spawn_points: Array[Marker3D]
+@export var respawn_time: float = 2.0
+#@onready var spawn_points: Array[Node] = get_tree().get_nodes_in_group("boss_chipling_spawn_marker")
 @export_group("Movement")
 @export var DESIRED_DISTANCE: float = 20.0
 @export var desired_distance: float = DESIRED_DISTANCE
+@export var MOVE_SPEED: float = 10.0
+@onready var move_speed: float = MOVE_SPEED:
+	set(value):
+		move_speed = value
+		navigation_component.current_speed = move_speed
 @export var wave_amplitude: float = 7.0
 @export var wave_frequency: float = 5.0
 @export var time_elapsed: float = 0.0
@@ -17,7 +26,7 @@ var center_pos: Vector3
 @export var orbit_angle: float = 0.0 # track this over time
 @export var orbit_radius: float = 40.0
 @export_subgroup("Wandering Movement")
-@export var wander_waypoints: Array[Marker3D] = []
+@export var wander_waypoints: Array[Node] = []
 @export var max_wander_distance: float = 20.0
 @export var max_waypoint_jitter_radius: float = 4.0
 @export var wander_delay_min: float = 0.5
@@ -26,28 +35,56 @@ var center_pos: Vector3
 @export var wander_timeout: float = 1.2
 @onready var wander_idle_timer: Timer = $IdleTimer
 @onready var wander_delay_timer: Timer = $WanderTimer
+var current_wander_target: Vector3
+@export_subgroup("Death Juice")
+@onready var chip_particles: GPUParticles3D = $ChipDeathParticles
+@export var explosion_particle_prefab: PackedScene
 
 var nav_map_rid: RID
 var nav_agent_rid: RID
 
 
 func _ready() -> void:
+	self.visible = false
 	GRAVITY = 0
 	super()
-	health_component.health_changed.connect(_on_health_changed)
 	
 	nav_map_rid = get_world_3d().get_navigation_map()
 	nav_agent_rid = NavigationServer3D.agent_create()
 	NavigationServer3D.agent_set_map(nav_agent_rid, nav_map_rid)
 
 
+func _physics_process(delta: float) -> void:
+	velocity.y -= GRAVITY * delta
+	time_elapsed += delta
+	var chase_direction: Vector3 = self.global_position.direction_to(current_wander_target)
+	var perpendicular: Vector3 = chase_direction.rotated(Vector3.UP, PI / 2)
+	var wave_offset = perpendicular * sin(time_elapsed * wave_frequency) * wave_amplitude
+	var desired_position = current_wander_target + wave_offset
+	navigation_component.set_nav_target_position(desired_position)
+	move_and_slide()
+
+
+func spawn() -> void:
+	state_chart.send_event("start_spawn")
+
+
 func _on_died() -> void:
 	died.emit()
+	sprite.visible = false
+	
 	health_component.show_damage_text = false
 	state_chart.send_event("death")
 	state_chart.send_event("stop_moving")
 	state_chart.send_event("deactivate")
 	await death_anim_finished
+	# TODO - add some juice, make the chipling explode with chips that have trails?
+	chip_particles.emitting = true
+	var explosion = explosion_particle_prefab.instantiate()
+	add_child(explosion)
+	await get_tree().create_timer(0.8).timeout
+	#
+	self.queue_free()
 
 
 func _on_passive_idle_state_entered() -> void:
@@ -72,9 +109,10 @@ func _on_passive_wander_state_entered() -> void:
 	var jittered_point: Vector3 = new_wander_point + offset_vector.rotated(
 		Vector3.UP, randf_range(0, 2 * PI)
 	)
-	draw_debug_sphere(new_wander_point, 0.5, Color.PURPLE)
-	draw_debug_sphere(jittered_point, 0.5, Color.MAGENTA)
-	navigation_component.set_nav_target_position(jittered_point)
+	#draw_debug_sphere(new_wander_point, 0.5, Color.PURPLE)
+	#draw_debug_sphere(jittered_point, 0.5, Color.MAGENTA)
+	current_wander_target = jittered_point
+	#navigation_component.set_nav_target_position(jittered_point)
 	wander_idle_timer.start(wander_timeout)
 
 
@@ -90,3 +128,20 @@ func _on_wander_timer_timeout() -> void:
 func _on_wander_idle_timer_timeout() -> void:
 	wander_delay_timer.stop()
 	state_chart.send_event("end_wander")
+
+
+func _on_spawning_idle_state_entered() -> void:
+	collider.disabled = false
+	GRAVITY = 0
+	state_chart.send_event("start_drop")
+
+
+func _on_spawning_dropping_state_entered() -> void:
+	self.visible = true
+	GRAVITY = 14
+
+
+func _on_spawning_dropping_state_physics_processing(delta: float) -> void:
+	if self.is_on_floor():
+		# TODO - add a bit of juice, squash/stretch and dust particles here?
+		state_chart.send_event("finish_spawn")
--- src/maps/tutorial/tutorial_boss_map.gd
+++ src/maps/tutorial/tutorial_boss_map.gd
@@ -2,11 +2,38 @@ extends BossMap
 
 @export var boss_doors: ElevatorDoors
 @export var exit_doors: ElevatorDoors
+@export var chipling_prefab: PackedScene
+@export var chiplings_to_spawn_1: int = 5
+@onready var chipling_spawns_1: Array[Node] = get_tree().get_nodes_in_group("boss_chipling_spawn_1_marker")
+@onready var chipling_wander_points_1: Array[Node] = get_tree().get_nodes_in_group("boss_chipling_wander_1_marker")
 
 
 func _ready() -> void:
 	super()
 	boss_doors.close()
+	for i in range(chiplings_to_spawn_1):
+		spawn_chipling_area_1()
+
+
+func spawn_chipling_area_1() -> void:
+	# Pick spawn point
+	var spawn_point = chipling_spawns_1.pick_random()
+	# Instance chipling
+	var chipling = chipling_prefab.instantiate()
+	chipling.wander_waypoints = chipling_wander_points_1
+	add_child(chipling)
+	chipling.global_position = spawn_point.global_position
+	chipling.died.connect(_on_chipling_died)
+	# Trigger chipling drop
+	#chipling.get_node("UI/StateChartDebugger").enabled = true
+	await chipling.ready
+	chipling.spawn()
+
+
+func _on_chipling_died() -> void:
+	await get_tree().create_timer(0.6).timeout
+	# TODO - make this generic for all spawning areas
+	spawn_chipling_area_1()
 
 
 func _on_boss_door_trigger_volume_body_entered(body: Node3D) -> void:
@@ -28,7 +55,10 @@ func _on_elevator_door_trigger_volume_body_exited(body: Node3D) -> void:
 	if body is Player:
 		exit_doors.close()
 
-
-func _on_debug_boss_trigger_body_entered(body: Node3D) -> void:
-	if body is Player:
-		pass
+#
+#func _on_debug_boss_trigger_body_entered(body: Node3D) -> void:
+	#if body is Player:
+		#for i in range(chiplings_to_spawn_1):
+			#spawn_chipling_area_1()
+			#await get_tree().create_timer(0.2).timeout
+	#$DEBUGBossTrigger.queue_free()
