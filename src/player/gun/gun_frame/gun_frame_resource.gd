73fe6cd3cd98892f6a4731bc5f0cef36ca8bd426
NKLam <khoalamvn@gmail.com>
2025-11-22 10:59:22 +0700

    Add gun frame resource

---
+++ src/player/gun/gun_frame/gun_frame_resource.gd
@@ -0,0 +1,30 @@
+extends Resource
+class_name GunFrameResource
+
+enum GunFrameIdEnum {
+	NONE,
+	SHOTGUN,
+	SMG,
+	SNIPER
+}
+
+@export var frame_name: String
+@export var frame_id: GunFrameIdEnum
+@export_group("Gun Properties")
+@export var base_damage: int = 20
+@export var base_projectile_amount: int = 1
+## Shot per second
+@export var base_firerate: float = 2
+@export var base_magazine_size: int = 10
+@export var base_reload_time: float = 1
+@export var base_spin_time: float = 1
+## How spread out projectile can be from the aim center
+@export var base_spread_angle: float = 0.5
+## Projectile dont have travel time. Shot enemy is instanly damaged. If this ticked, ignore projectile_speed
+@export var is_hitscan: bool
+## How fast projectile travel. Ignored if is_hitscan ticked. Shouldn't higher than 100 or collision detecion
+## will be an issue
+@export var base_projectile_speed: float = 50
+@export var recoil_amount: float = 0.03
+## How much screenshake when player shot
+@export var screenshake_amount: float = 0.2
