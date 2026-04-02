fe4ff58b1ede90c9b8a7d9bf97cd89e98d77bcc2
Jack McCaffrey <jackmccaffrey96@gmail.com>
2026-04-02 17:08:19 +0100

    Made luck gain messages stack for cleaner UI

--- src/ui/player/luck/luck_message_ui.gd
+++ src/ui/player/luck/luck_message_ui.gd
@@ -4,6 +4,7 @@ extends Control
 @export var luck_popup_lifetime: float = 2.0
 
 @onready var luck_popup_parent: VBoxContainer = $VBoxContainer
+var active_luck_popups: Array[LuckMessagePopupUI] = []
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,6 +13,20 @@ func _ready() -> void:
 
 
 func _on_luck_message(message: String, _is_gain: bool) -> void:
+	# Check to see if we have an existing version of this message
+	for popup in active_luck_popups:
+		if popup.text == message:
+			popup.change_count(1)
+			return
+	
 	var _new_message_ui = luck_popup_ui.instantiate()
+	
 	luck_popup_parent.add_child(_new_message_ui)
-	_new_message_ui.luck_popup(message, luck_popup_lifetime)
+	_new_message_ui.text = message
+	_new_message_ui.lifetime = luck_popup_lifetime
+	active_luck_popups.append(_new_message_ui)
+	_new_message_ui.popup_expired.connect(
+		func(popup): active_luck_popups.erase(popup)
+	)
+	
+	_new_message_ui.show_popup()
