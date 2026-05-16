2eda56de56140c4a2952943a174118c30f9197f1
NKLam <khoalamvn@gmail.com>
2026-05-16 13:11:06 +0700

    Fix negative focus_idx

--- src/ui/player/inventory/inventory_ui.gd
+++ src/ui/player/inventory/inventory_ui.gd
@@ -57,7 +57,9 @@ func _input(event: InputEvent) -> void:
 			return
 	
 	if visible:
-		var focused_ui: Control = current_focus_area.get_child(active_focus_idx) if current_focus_area else null
+		var focused_ui: Control = null
+		if active_focus_idx >= 0 and current_focus_area:
+			focused_ui = current_focus_area.get_child(active_focus_idx)
 		
 		if event.is_action_pressed("interact"):
 			close()
