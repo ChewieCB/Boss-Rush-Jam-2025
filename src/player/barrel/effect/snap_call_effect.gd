c1c9887feda0cbc6328a25af5558c30228117a2f
NKLam <khoalamvn@gmail.com>
2026-08-15 02:28:10 +0700

    Add snap call bonus projectile speed based on player speed

--- src/player/barrel/effect/snap_call_effect.gd
+++ src/player/barrel/effect/snap_call_effect.gd
@@ -6,7 +6,20 @@ extends BaseBarrelEffect
 ## How much dmg projectile is modified depend on difference between this 
 ## and threshold, per godot's unit
 @export var dmg_modify_perc_per_unit: float = 1
-# @export var dmg_modify_for_hitscan: float = 2.0
+
+@export var player_speed_bonus_mult = 4
+@export var player_dash_mult = 2
+
+const HIGH_SPEED_THRESHOLD = 100
+
+
+func on_projectile_spawn(projectile: BaseBullet):
+	# Add player current speed to projectile
+	var bonus_speed = GameManager.player.vel_horizontal.length() * player_speed_bonus_mult
+	if GameManager.player.is_dashing:
+		bonus_speed *= player_dash_mult
+	projectile.projectile_speed += bonus_speed
+
 
 func on_before_damage_applied(_enemy: CharacterBody3D, projectile: BaseBullet):
 	# If hitscan, just ignore it
