3604801da45eccd5b024e1d49cbf2532236ea7ba
NKLam <khoalamvn@gmail.com>
2026-09-21 20:47:41 +0700

    Remove unused ramp up code

--- src/player/barrel/effect/ramp_up_effect.gd
+++ src/player/barrel/effect/ramp_up_effect.gd
@@ -88,20 +88,3 @@ func on_fire_rate_check():
 	last_applied_delta = new_firerate - base_firerate
 	gun.modified_firerate = new_firerate
 	last_written_firerate = new_firerate
-
-
-func on_barrel_start_spin():
-	remove_effect()
-
-func on_barrel_stop_spin():
-	start_effect()
-
-func remove_effect():
-	luck_trigger_timer.stop()
-
-func start_effect():
-	luck_trigger_timer.start()
-
-
-func _on_timer_timeout() -> void:
-	pass # Replace with function body.
